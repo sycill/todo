27b1ec51c61f68253f00b3e75a20806d2fbbbde9
CéliaBOUCHAUT <celia.bouchaut@gmail.com>
2022-09-29 21:31:17 +0200

    ajout de boutons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const task = document.querySelector('input');
+const button = document.querySelector('button');
+let list_tasks = [];
+
+button.addEventListener('click', add);
+
+add(){
+    if(!task){
+        list_tasks.style.
+    }
+}
